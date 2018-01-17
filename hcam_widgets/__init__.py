f7eb168e7c2c99710eda672759e251d270daeab2
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-01-17 09:44:24 +0000

    Bump version: 0.4.0 → 0.5.0

--- hcam_widgets/__init__.py
+++ hcam_widgets/__init__.py
@@ -3,7 +3,7 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 
 __author__ = """Stuart Littlefair"""
 __email__ = 's.littlefair@shef.ac.uk'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 
 class DriverError(Exception):
